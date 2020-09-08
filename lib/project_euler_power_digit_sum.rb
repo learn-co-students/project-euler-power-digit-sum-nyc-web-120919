b95c536549bf0614ba5f7a579a32e20009f78576
flatironschool <danliu277@gmail.com>
2020-09-08 13:31:38 -0400

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,9 @@
 # code your solution here
+def power_digit_sum(x, n)
+    num = x**n
+    result = 0
+    num.to_s.split('').each do |digit|
+        result += digit.to_i
+    end
+    return result
+end
